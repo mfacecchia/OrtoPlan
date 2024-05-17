d0f9fc648f43cd924291fb1db6222e90d7b5cbf4
facecchiamarco <facecchiamarco@icloud.com>
2024-05-17 15:12:54 +0200

    backend-configuration: initial node express configuration

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+app.use(cors(
+    {
+        // Frontend address
+        origin: 'http://localhost:5500/'
+    }
+));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/ping', (req, res) => {
+    res.status(200).json({
+        'status': "200 OK",
+        "message": "Pong"
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Listening");
+});
