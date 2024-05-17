101aeb2350904cf8acca4c96bce4e2be60b3a77a
facecchiamarco <facecchiamarco@icloud.com>
2024-05-17 13:06:09 +0200

    frontend-configuration: initial express configuration

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
+
+app.listen(5500, () => {
+    console.log("Pong.");
+});
