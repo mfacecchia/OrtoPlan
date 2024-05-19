1d4be1d3e57593a748acd416a002a39dba8055cb
facecchiamarco <facecchiamarco@icloud.com>
2024-05-19 18:47:05 +0200

    Frontend: add custom theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,31 @@ module.exports = {
     'src/*.js',
     'public/script/*.js'
   ],
+  daisyui: {
+    themes: [
+      {
+        ortoPlan: {
+          "primary": "#7FBA52",
+                  
+          "secondary": "#B2D697",
+                  
+          "accent": "#634E43",
+                  
+          "neutral": "#808080",
+                  
+          "base-100": "#FFFFFF",
+                  
+          "info": "#4F6BCE",
+                  
+          "success": "#28A34C",
+                  
+          "warning": "#D2D551",
+                  
+          "error": "#CE4F4F",
+        },
+      },
+    ]
+  },
   theme: {
     extend: {}
   },
