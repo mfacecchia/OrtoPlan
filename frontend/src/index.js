9b8155a672200671224f7d87d7e434b2128b337a
facecchiamarco <facecchiamarco@icloud.com>
2024-05-21 23:14:22 +0200

    Frontend: add plantation view & relative route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,7 +19,15 @@ app.get('/signup', (req, res) => {
 
 app.get('/user/plantations', (req, res) => {
     res.render('pages/plantationsScreening');
-})
+});
+
+app.get('/user/plantations/:plantationID', (req, res) => {
+    /* 
+        * TODO: validate the plantation id by making a requesto to the backend
+        * before rendering the actual page 
+    */
+    res.render('pages/plantation');
+});
 
 app.listen(5500, () => {
     console.log("Pong.");
