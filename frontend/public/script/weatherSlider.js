c3c876941818e715f43f795c465d90f925a16a17
facecchiamarco <facecchiamarco@icloud.com>
2024-05-24 11:02:56 +0200

    Frontend: add weather slider & relative icons

---
+++ frontend/public/script/weatherSlider.js
@@ -0,0 +1,9 @@
+const weatherSlider = new Glide('#weatherSlider', {
+    perView: 2,
+    animationDuration: 700,
+    breakpoints: {
+        // Slider responsive options
+    }
+});
+
+weatherSlider.mount();
