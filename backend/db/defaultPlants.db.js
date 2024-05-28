8bb5e7326606d10c714a7b4a69c6ff0514ddc778
facecchiamarco <facecchiamarco@icloud.com>
2024-05-28 18:14:54 +0200

    Database: add script to compile plants' DB table with some default plants obtained from API

---
+++ backend/db/defaultPlants.db.js
@@ -0,0 +1,81 @@
+import { PrismaClient } from "@prisma/client";
+import fetch from "node-fetch";
+import "dotenv/config";
+import prisma from './prisma.db.js'
+
+
+async function getPlantsInfo(){
+    console.log("Obtaining plants");
+    try{
+        const allPlants = await fetch('https://house-plants2.p.rapidapi.com/all-lite', {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+                'X-RapidAPI-Host': 'house-plants2.p.rapidapi.com'
+            }
+        });
+        if(!allPlants.ok) throw new Error(`Server responded with a status code of ${allPlants.status}`);
+        const allPlantsJson = await allPlants.json();
+        console.log("Done.");
+        const filteredPlants = await filterPlants(allPlantsJson);
+        console.log(`${filteredPlants.length} plants successfully filtered.`);
+        console.log("Adding plants to DB");
+        // TODO: Add all plants to DB
+        console.log("Done.");
+    }catch(err){
+        console.log(err);
+    }
+}
+
+async function filterPlants(plantsList){
+    const filteredPlants = [];
+    console.log("Filtering plants");
+    for(const plant of plantsList){
+        const plantName = plant['Common name'];
+        // Skipping if the plant has no name
+        console.log(typeof plantName);
+        if(!plantName) continue;
+        let plantImg = '';
+        try{
+            plantImg = await getPlantImage(plantName);
+        }catch(err){
+            break;
+        }
+        filteredPlants.push({
+            plantName: plantName[0],
+            plantFamily: plant['Family'],
+            plantDescription: `{"Category": "${plant['Categories']}", "Variety": "${plant['Origin']}", "Preferred Climate": "${plant['Climat']}"}`,
+            scientificName: plant['Latin name'],
+            imageURL: plantImg
+        });
+    }
+    console.log("Done.");
+    return filteredPlants;
+}
+
+function getPlantImage(plantName){
+    /*
+        * Obtains plant image from given plant name
+        * Uses Unsplash Images API (docs here => https://unsplash.com/documentation)
+    */
+    console.log(`Finding image for ${plantName}`);
+    return new Promise(async (resolve, reject) => {
+        try{
+            const res = await fetch(`https://api.unsplash.com/search/photos?query=${encodeURI(plantName + ' Plant')}`, {
+                method: 'GET',
+                headers: {
+                    "Authorization": `Client-ID ${process.env.UNSPLASHAPI_KEY}`,
+                    "Accept-Version": "v1"
+                }
+            });
+            console.log(`Completed with ${res.status} status code.`);
+            if(!res.ok) reject(res.status);
+            const imageJSON = await res.json();
+            resolve(imageJSON.results[0].urls.regular);
+        }catch(err){
+            reject(err);
+        }
+    });
+}
+
+getPlantsInfo();
