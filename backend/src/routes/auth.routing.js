e68d9ef891952ce6c722897001e96f639d60ec73
facecchiamarco <facecchiamarco@icloud.com>
2024-05-29 12:04:21 +0200

    Backend: introduce user login functionality

--- backend/src/routes/auth.routing.js
+++ backend/src/routes/auth.routing.js
@@ -26,6 +26,7 @@ export default function userAuth(app){
         try{
             const hashedPass = await hashPassword(req.body.password);
             await newUser(req.body, hashedPass);
+            // TODO: Change to 201
             res.status(200).json({
                 status: 200,
                 message: "User created successfully"
@@ -34,6 +35,24 @@ export default function userAuth(app){
             res.status(400).json({
                 status: 400,
                 message: "Unknown error. Please try again later."
+            });
+        }
+    });
+
+    app.post('/user/login', async (req, res) => {
+        try{
+            const userExists = await findUser(req.body.email);
+            const verified = await argon2.verify(userExists.password, req.body.password);
+            if(!verified) throw new Error(false);
+            // TODO: Generate JWT
+            res.status(200).json({
+                status: 200,
+                message: "Logged in successfully.",
+            });
+        }catch(err){
+            res.status(404).json({
+                status : 404,
+                message: "User not found. Please try again."
             });
         }
     });
@@ -68,6 +87,25 @@ function hashPassword(clearPass){
             resolve(hashedPass);
         }catch(err){
             reject('Could not hash password.');
+        }
+    });
+}
+
+async function findUser(userEmail, throwOnFound = false){
+    /*
+        * Finds a user from the database by a given valid `userEmail`
+        * Returns a `Promise` that `reject`s if the `throwOnFound` is set to `true`, otherwise `resolve`s
+    */
+    return new Promise(async (resolve, reject) => {
+        try{
+            const userExists = await prisma.credentials.findUniqueOrThrow({
+                where: {
+                    email: userEmail
+                }
+            });
+            throwOnFound? reject(false): resolve(userExists);
+        }catch(err){
+            throwOnFound? resolve(userExists): reject(false);
         }
     });
 }
