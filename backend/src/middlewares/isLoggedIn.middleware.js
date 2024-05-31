5ff84aecd578542d13d3570f11a7ea27af818484
facecchiamarco <facecchiamarco@icloud.com>
2024-05-31 12:07:54 +0200

    Backend: update user login state middleware to check if the suer exists after validating the JWT

--- backend/src/middlewares/isLoggedIn.middleware.js
+++ backend/src/middlewares/isLoggedIn.middleware.js
@@ -1,4 +1,6 @@
 import { validateJWT } from "../auth/jwt.auth.js";
+import { findUser } from "./findUser.middleware.js";
+import jwt from 'jsonwebtoken'
 
 
 export const isLoggedIn = () => {
@@ -11,6 +13,9 @@ export const isLoggedIn = () => {
             const token = req.headers.authorization;
             if(token){
                 await validateJWT(token.replace('Bearer ', ''));
+                // Obtaining the userID from the jwt decoded payload to check if the user still exists
+                const tokenUserID = jwt.decode(token.replace('Bearer ', '')).userID;
+                await findUser(tokenUserID, true, false)
                 res.status(200).json({
                     status: 200,
                     message: "Logged in successfully."
