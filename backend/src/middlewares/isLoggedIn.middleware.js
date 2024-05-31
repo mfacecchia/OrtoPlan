b3b233c008cfe482fc6007b514ac7885af8e1435
facecchiamarco <facecchiamarco@icloud.com>
2024-06-01 00:44:27 +0200

    Backend: update user login state middleware with strict parameter error management

--- backend/src/middlewares/isLoggedIn.middleware.js
+++ backend/src/middlewares/isLoggedIn.middleware.js
@@ -16,6 +16,7 @@ export const isLoggedIn = (strict = false) => {
         */
         try{
             const token = req.headers.authorization;
+            if(strict && !token) throw new Error('Invalid token.');
             if(token){
                 await validateJWT(token.replace('Bearer ', ''));
                 // Obtaining the userID from the jwt decoded payload to check if the user still exists
