15d794591ee4f4e959e886be5d2abcd32896fb0c
facecchiamarco <facecchiamarco@icloud.com>
2024-05-31 04:05:45 +0200

    Backend: update find user middleware by allowing user search for email and userID

--- backend/src/middlewares/findUser.middleware.js
+++ backend/src/middlewares/findUser.middleware.js
@@ -1,21 +1,30 @@
 import prisma from '../../db/prisma.db.js'
 
-// TODO: Change `userEmail` to `userEmailOrID` and check for user id first and user id then
-export default async function findUser(userEmail, throwOnFound = false){
+
+export default async function findUser(userEmailOrID, throwOnFound = false){
     /*
-        * Finds a user from the database by a given valid `userEmail`
+        * Finds a user from the database by a given valid `userEmailOrID`
         * Returns a `Promise` that `reject`s if the `throwOnFound` param is set to `true`, otherwise `resolve`s
     */
     return new Promise(async (resolve, reject) => {
         let userExists = undefined;
+        let dbSelector = undefined
+        // Defining the right database field to get the value from based on the `userEmailOrID` param type to allow selection between userID or email
+        if(typeof userEmailOrID === 'string'){
+            dbSelector = { email: userEmailOrID }
+        }
+        else{
+            dbSelector = { userID: userEmailOrID }
+        }
         try{
             userExists = await prisma.credentials.findUniqueOrThrow({
                 where: {
-                    email: userEmail
+                    ...dbSelector
                 }
             });
             throwOnFound? reject(false): resolve(userExists);
         }catch(err){
+            console.log(err);
             throwOnFound? resolve(userExists): reject(false);
         }
     });
