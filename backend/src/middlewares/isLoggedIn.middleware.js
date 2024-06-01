46ec0a3b327d5be6e995f503b5eb60809e234ee7
facecchiamarco <facecchiamarco@icloud.com>
2024-06-01 10:08:16 +0200

    Backend: update user login state middleware with possibility to set header on valid token or pass to the next middleware

--- backend/src/middlewares/isLoggedIn.middleware.js
+++ backend/src/middlewares/isLoggedIn.middleware.js
@@ -3,17 +3,13 @@ import { findUser } from "./findUser.middleware.js";
 import jwt from 'jsonwebtoken'
 
 
-export const isLoggedIn = (strict = false) => {
+export const isLoggedIn = (strict = false, setHeaderOnValid = true) => {
     /*
         * Checks if the user is already logged in by validating the Bearer tokn passed in the request header
         * Returns a 401 Unauthorized response status if the token is nto valid, otherwise calls the `next()` middleware
         * The `strict` parameter can be set to `true` in case you want to send 401 status code if the token does not exist as well
     */
     return async (req, res, next) => {
-        /*
-            * Checks if the user is logged in by checking for token presence in the Authorization header and eventual validity
-            * Otherwise checks if the request body contains email/password parameters to pass to the next middleware
-        */
         try{
             const token = req.headers.authorization;
             if(strict && !token) throw new Error('Invalid token.');
@@ -22,11 +18,13 @@ export const isLoggedIn = (strict = false) => {
                 // Obtaining the userID from the jwt decoded payload to check if the user still exists
                 const tokenUserID = jwt.decode(token.replace('Bearer ', '')).userID;
                 await findUser(tokenUserID, true, false)
-                res.status(200).json({
-                    status: 200,
-                    message: "Logged in successfully."
-                });
-                return;
+                if(setHeaderOnValid){
+                    res.status(200).json({
+                        status: 200,
+                        message: "Logged in successfully."
+                    });
+                    return;
+                }
             }
         }catch(err){
             res.status(401).json({
