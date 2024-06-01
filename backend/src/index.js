e469fb149883441726c2cd0c956af135861011c0
facecchiamarco <facecchiamarco@icloud.com>
2024-06-01 10:33:07 +0200

    Backend: move forecast endpoints to routes directory

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import userAuth from './routes/auth.routing.js'
-import getWeatherInfo from './apis/getWeather.api.js'
-import { isLoggedIn } from './middlewares/isLoggedIn.middleware.js';
+import forecast from './routes/weather.routing.js'
 
 const app = express();
 app.use(cors(
@@ -22,28 +21,9 @@ app.get('/ping', (req, res) => {
     });
 });
 
-app.post('/api/weather', isLoggedIn(), async (req, res) => {
-    if(typeof req.body.location === undefined) res.status(400).json({
-        status: "400",
-        message: "Unable to process the request. Location not provided."
-    });
-    try{
-        const forecast = await getWeatherInfo(req.body.location);
-        res.status(200).json({
-            status: "200",
-            message: "Forecast obtained successfully",
-            forecast: forecast
-        });
-    }catch(err){
-        res.status(404).json({
-            status: "404",
-            message: err.message || err
-        });
-    }
-});
-
 app.listen(process.env.PORT, () => {
     console.log("Listening");
 });
 
 userAuth(app);
+forecast(app);
