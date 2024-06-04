d7d7322d7c0ad25d0e878c125aafb44faa0a8a13
facecchiamarco <facecchiamarco@icloud.com>
2024-06-04 17:27:10 +0200

    Backend: add function to get location data based on location name and code from the DB

---
+++ backend/src/apis/getLocation.api.js
@@ -0,0 +1,17 @@
+import prisma from "../../db/prisma.db.js";
+
+export default function getLocation(locationName, locationCAP = undefined){
+    // Defining all parameters to use as query `where` clause (location name as default and locationCAP in case `locationCAP` is passed)
+    const queryParams = { locationName: locationName }
+    if(typeof locationCAP !== undefined) queryParams.locationCAP = locationCAP
+    return new Promise(async (resolve, reject) => {
+        try{
+            const location = await prisma.location.findFirstOrThrow({
+                where: queryParams
+            });
+            resolve(location);
+        }catch(err){
+            reject('Location not found');
+        }
+    });
+}
