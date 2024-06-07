3c3b577e77eced986d1fb0d2fce759b9daba8d4f
facecchiamarco <facecchiamarco@icloud.com>
2024-06-07 20:16:41 +0200

    Validation: add treatments validation script

---
+++ backend/src/validation/treatments.validation.js
@@ -0,0 +1,63 @@
+import validate from 'validate.js';
+import { defaultPresenceValidator } from './customDefaultValidators.validation.js';
+import moment from 'moment';
+
+
+export const validateTreatment = () => {
+    return async (req, res, next) => {
+        validate.extend(validate.validators.datetime, {
+            // Parsing date for validation
+            parse: value => {
+                return +moment.utc(value, true)
+            },
+            // Parsing date to display in the message in case of validation errors
+            format: value => {
+                return moment(value).format('YYYY-MM-DD');
+            }
+        });
+        try{
+            await validate.async(req.body, {
+                treatmentType: {
+                    ...defaultPresenceValidator,
+                    inclusion: {
+                        within: ['Irrigation', 'Sowing', 'Pruning', 'Fertilization'],
+                        message: '^Must be between \'Irrigation\', \'Sowing\', \'Pruning\', and \'Fertilization\''
+                    }
+                },
+                notes: {
+                    length: {
+                        maximum: 300,
+                        tooLong: '^Too long (maximum length is %{count} characters).'
+                    }
+                },
+                treatmentDate: {
+                    ...defaultPresenceValidator,
+                    datetime: {
+                        earliest: moment.utc(),
+                        tooEarly: '^Planned date must be today or later'
+                    }
+                },
+                treatmentRecurrence: {
+                    ...defaultPresenceValidator,
+                    numericality: {
+                        greaterThanOrEqualTo: 0,
+                        onlyInteger: true,
+                        notInteger: '^Must be an integer',
+                        notGreaterThanOrEqualTo: '^Must be equal or greater than 0'
+                    }
+                }
+            });
+            req.body.treatmentID = parseInt(req.body.treatmentID) || 0;
+            req.body.plantationPlantID = parseInt(req.body.plantationPlantID) || 0;
+            req.body.treatmentRecurrence = parseInt(req.body.treatmentRecurrence) || 0;
+            req.body.treatmentDate = moment(req.body.treatmentDate, true)
+            next();
+        }catch(validationErrors){
+            res.status(403).json({
+                status: 403,
+                message: 'Invalid values. Please try again.',
+                validationErrors: validationErrors
+            });
+        }
+    }
+}
