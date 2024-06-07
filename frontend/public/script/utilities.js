cc71cde99451adec90f602f32a2cb31c6485435a
facecchiamarco <facecchiamarco@icloud.com>
2024-06-07 12:29:35 +0200

    Frontend: add functions to manage tab-focusable elements in dialogs on close and open

--- frontend/public/script/utilities.js
+++ frontend/public/script/utilities.js
@@ -101,3 +101,23 @@ function formDataToObject(formDataElement, fieldsFilterArr = undefined){
     }
     return obj;
 }
+
+function setTabIndexToZero(dialog){
+    /*
+        * Makes all the tab focusable elements actually focusable by setting their `tabindex` value to 0
+        * Can be used bundled with dialog `showDialog` function
+    */
+    dialog.querySelectorAll('[tabindex="-1"]').forEach(element => {
+        element.setAttribute('tabindex', '0');
+    });
+}
+
+function setTabIndexToMinusOne(dialog){
+    /*
+        * "Removes" all the tab focusable elements property by setting their `tabindex` value to -1
+        * Can be used bundled with dialog `close` event listener
+    */
+    dialog.querySelectorAll('[tabindex="0"]').forEach(element => {
+        element.setAttribute('tabindex', '-1');
+    });
+}
