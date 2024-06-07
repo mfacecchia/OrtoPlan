3b8e83ead6c50fe923eba6480a23c5df2eb4c833
facecchiamarco <facecchiamarco@icloud.com>
2024-06-07 18:21:39 +0200

    Validation: add custom default validator for prisma varChar max length chars count & add message to defaultPresenceValidator

--- backend/src/validation/customDefaultValidators.validation.js
+++ backend/src/validation/customDefaultValidators.validation.js
@@ -1,2 +1,3 @@
 // Default validator for empty values, defined to simplify code readability
-export const defaultPresenceValidator = { presence: { allowEmpty: false } };
+export const defaultPresenceValidator = { presence: { allowEmpty: false, message: "^This field is required." } };
+export const defaultPrismaMaxLength = { length: { maximum: 191, tooLong: '^Too long (maximum length is %{count} characters).' } }
