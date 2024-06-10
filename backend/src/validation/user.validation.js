a3144f13fd9f2a153c496326a2f89749574c43f7
facecchiamarco <facecchiamarco@icloud.com>
2024-06-10 17:42:28 +0200

    Validation: update user validation script for user update functionality

--- backend/src/validation/user.validation.js
+++ backend/src/validation/user.validation.js
@@ -1,6 +1,8 @@
 import { findUser } from '../middlewares/findUser.middleware.js';
-import validate from 'validate.js'
+import validate from 'validate.js';
 import { defaultPresenceValidator, defaultPrismaMaxLength } from './customDefaultValidators.validation.js';
+import argon2 from 'argon2';
+import decodeToken from '../jwt/decode.jwt.js';
 
 
 export const validateLoginSignup = (isLogin = true) => {
@@ -22,7 +24,7 @@ export const validateLoginSignup = (isLogin = true) => {
         // Additional validators in case of user sign-up
         if(!isLogin){
             // Creating a custom validator to check if the email already exists and applying it to the email validators Object
-            validate.validators.emailExists = async (value) => {
+            validate.validators.emailExists = (value) => {
                 return new Promise(async (resolve, reject) => {
                     try{
                         await findUser(value, false, true);
@@ -58,7 +60,88 @@ export const validateLoginSignup = (isLogin = true) => {
             }
             next();
         }catch(validationErrors){
-            console.log(validationErrors);
+            res.status(403).json({
+                status: 403,
+                message: 'Invalid values. Please try again.',
+                validationErrors: validationErrors
+            });
+        }
+    }
+}
+
+export const validateUserUpdate = () => {
+    return async(req, res, next) => {
+        const validators = {
+            firstName: {
+                ...defaultPresenceValidator,
+                ...defaultPrismaMaxLength
+            },
+            lastName: {
+                ...defaultPresenceValidator,
+                ...defaultPrismaMaxLength
+            },
+            email: {
+                ...defaultPresenceValidator,
+                ...defaultPrismaMaxLength,
+                email: true,
+                emailExists: true
+            }
+        };
+        if(req.body.password){
+            validators.oldPassword = {
+                ...defaultPresenceValidator,
+                oldPasswordMatches: {
+                    email: req.body.email
+                }
+            };
+            validators.password = {
+                ...defaultPresenceValidator,
+                length: {
+                    minimum: 15,
+                    tooShort: '^Too short (minimum length is %{count} characters).'
+                }
+            };
+            validators.passwordVerify = {
+                ...defaultPresenceValidator,
+                equality: {
+                    attribute: 'password',
+                    message: '^Passwords do not match'
+                }
+            };
+        };
+        validate.validators.email.message = '^Not a valid email';
+        // Creating a custom validator to check if the email already exists and applying it to the email validators Object
+        validate.validators.emailExists = (value) => {
+            return new Promise(async (resolve, reject) => {
+                try{
+                    await findUser(value, false, true);
+                    resolve();
+                }catch(err){
+                    // If the user is found, then check if the corresponsing userID is different to the user who is making the actual request
+                    const userID = decodeToken(req.headers.authorization).userID;
+                    if(userID === err.userID) resolve();
+                    else resolve('^Email already in use.');
+                }
+            });
+        }
+        validate.validators.oldPasswordMatches = async (value, options) => {
+            return new Promise(async (resolve, reject) => {
+                try{
+                    const userCredentials = await findUser(options.email);
+                    if(await argon2.verify(userCredentials.password, value)) resolve();
+                    else throw new Error();
+                }catch(err){
+                    resolve('^Invalid password or user not found');
+                }
+            });
+        };
+        try{
+            await validate.async(req.body, validators);
+            req.body.email = req.body.email.toLowerCase();
+            req.body.firstName = validate.capitalize(req.body.firstName.trim());
+            req.body.lastName = validate.capitalize(req.body.lastName.trim());
+            next();
+        }catch(validationErrors){
             res.status(403).json({
                 status: 403,
                 message: 'Invalid values. Please try again.',
