4ead6a380915782f943f121d8617b90ea70b694f
facecchiamarco <facecchiamarco@icloud.com>
2024-06-11 21:16:01 +0200

    Frontend: add function to remove user's notifications from db by server endpoint call

--- frontend/public/script/apis/notifications.api.js
+++ frontend/public/script/apis/notifications.api.js
@@ -14,3 +14,20 @@ async function getNotificationsList(){
         return false;
     }
 }
+
+async function removeAllNotifications(){
+    try{
+        const res = await fetch(`${BACKEND_ADDRESS}/api/notifications/all`, {
+            method: 'DELETE',
+            headers: {
+                "Accept": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem('OPToken')}`
+            }
+        });
+        const jsonRes = await res.json();
+        if(!res.ok) throw new Error(jsonRes.message);
+        return jsonRes.treatments;
+    }catch(err){
+        return false;
+    }
+}
