3b8803d3ceaea0f589e8ca1925bad6c06a27f43c
facecchiamarco <facecchiamarco@icloud.com>
2024-06-11 21:14:21 +0200

    Frontend: add function to retrieve notifications from server

---
+++ frontend/public/script/apis/notifications.api.js
@@ -0,0 +1,16 @@
+async function getNotificationsList(){
+    try{
+        const res = await fetch(`${BACKEND_ADDRESS}/api/notifications/all`, {
+            method: 'GET',
+            headers: {
+                "Accept": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem('OPToken')}`
+            }
+        });
+        const jsonRes = await res.json();
+        if(!res.ok) throw new Error(jsonRes.message);
+        return jsonRes.treatments;
+    }catch(err){
+        return false;
+    }
+}
