f37e4fa2afe436bd5d4f7493ff2427cb6e495d7f
facecchiamarco <facecchiamarco@icloud.com>
2024-06-11 20:41:57 +0200

    Backend: add notification POST endpoint

--- backend/src/routes/notifications.routing.js
+++ backend/src/routes/notifications.routing.js
@@ -1,5 +1,7 @@
+import prisma from '../../db/prisma.db.js';
 import decodeToken from '../jwt/decode.jwt.js';
 import { getUserNotification } from '../apis/getUserNotifications.api.js';
+import { validateNotification } from '../validation/notification.validation.js';
 
 
 export default function notifications(app){
@@ -22,5 +24,37 @@ export default function notifications(app){
                     message: "No notifications found"
                 });
             }
-        });
+        })
+        .post(validateNotification(), async (req, res) => {
+            const decodedToken = decodeToken(req.headers.authorization, false);
+            req.body.userID = decodedToken.userID;
+            try{
+                const newPlantation = await createNotification(req.body);
+                res.status(201).json({
+                    status: 201,
+                    message: "Plantation successfully created",
+                    plantation: newPlantation
+                });
+            }catch(err){
+                res.status(400).json({
+                    status: 400,
+                    message: "Unknown error while creating the notification. Please try again."
+                });
+                return;
+            }
+        })
+}
+
+
+function createNotification(notificationData){
+    return new Promise(async (resolve, reject) => {
+        try{
+            const newNotification = await prisma.notification.create({
+                data: notificationData
+            });
+            resolve(newNotification);
+        }catch(err){
+            reject(false);
+        }
+    });
 }
