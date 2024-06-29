76e861858ca6f3f0ddf5413c2c28f5969d1732b1
facecchiamarco <facecchiamarco@icloud.com>
2024-06-29 13:05:34 +0200

    Backend: add return token in form of link boolean parameter in email verification link generator function

--- backend/src/mail/validateAddress.mail.js
+++ backend/src/mail/validateAddress.mail.js
@@ -3,13 +3,14 @@ import prisma from "../../db/prisma.db.js";
 import 'dotenv/config';
 
 
-export default async function generateEmailVerificationLink(userEmail){
+export default async function generateEmailVerificationLink(userEmail, returnLink = false){
     const messageID = await createMessage(userEmail);
     const token = jwt.sign({
         messageID: messageID.messageID
     }, process.env.JWT_MAIL_VERIFICATION_SECRET, {
         expiresIn: '10m'
     });
+    if(returnLink) return `${process.env.FRONTEND_ADDRESS + ':' + process.env.FRONTEND_PORT}/user/verify?q=${token}`;
     return token;
 }
 
