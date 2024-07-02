6b43a95134fc07a67daa7cea51dc71e6d3955d8d
facecchiamarco <facecchiamarco@icloud.com>
2024-07-02 20:51:26 +0200

    Backend: update isLoggedIn middleware to set user key/value pair in request Object

--- backend/src/middlewares/isLoggedIn.middleware.js
+++ backend/src/middlewares/isLoggedIn.middleware.js
@@ -3,11 +3,13 @@ import { findUser } from "../apis/findUser.api.js";
 import jwt from 'jsonwebtoken'
 
 
-export const isLoggedIn = (strict = false, setHeaderOnValid = true) => {
+export const isLoggedIn = (strict = false, setHeaderOnValid = true, returnLastUserValues = false) => {
     /*
         * Checks if the user is already logged in by validating the Bearer tokn passed in the request header
         * Returns a 401 Unauthorized response status if the token is nto valid, otherwise calls the `next()` middleware
         * The `strict` parameter can be set to `true` in case you want to send 401 status code if the token does not exist as well
+        * The `returnLastUserValues` is used to keep track and return the values obtained from the `findUser()` function;
+        * this is useful in case we want to check if the email has changed throughout an eventual user credentials update
     */
     return async (req, res, next) => {
         try{
@@ -20,6 +22,7 @@ export const isLoggedIn = (strict = false, setHeaderOnValid = true) => {
                 const user = await findUser(tokenUserID, true, false);
                 // NOTE: Passing the user's email verification status to the next middleware for further verifications
                 req.userEmailStatus = user.verified;
+                if(returnLastUserValues) req.lastUserValues = user;
                 if(setHeaderOnValid){
                     res.status(200).json({
                         status: 200,
