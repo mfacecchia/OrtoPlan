6e9fc5c95d56967adcbffc1e2d5b041be7f4f742
facecchiamarco <facecchiamarco@icloud.com>
2024-07-09 15:22:17 +0200

    Backend: update JWT blacklist script with function to check if a certain token is blacklisted

--- backend/src/jwt/blacklist.jwt.js
+++ backend/src/jwt/blacklist.jwt.js
@@ -1,10 +1,24 @@
 import prisma from "../../db/prisma.db.js";
 
-export default async function blacklistToken(token){
+export async function blacklistToken(token){
     if(token.includes('Bearer ')) token = token.replace('Bearer ', '');
     try{
         const blacklistedToken = await prisma.jWTBlacklist.create({
             data: {
+                token: token
+            }
+        });
+        return blacklistedToken;
+    }catch(err){
+        return false;
+    }
+}
+
+export async function isTokenBlacklisted(token){
+    if(token.includes('Bearer ')) token = token.replace('Bearer ', '');
+    try{
+        const blacklistedToken = await prisma.jWTBlacklist.findUniqueOrThrow({
+            where: {
                 token: token
             }
         });
