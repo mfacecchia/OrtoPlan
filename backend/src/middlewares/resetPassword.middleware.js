5342d4441b79ae7689682d4eb2037422da7004c8
facecchiamarco <facecchiamarco@icloud.com>
2024-07-11 16:38:34 +0200

    Backend: move password reset token validation to an external middleware function

---
+++ backend/src/middlewares/resetPassword.middleware.js
@@ -0,0 +1,18 @@
+import { validateJWT } from "../auth/jwt.auth.js";
+import 'dotenv/config';
+
+
+export default function isResetTokenValid(){
+    return async (req, res, next) => {
+        try{
+            await validateJWT(req.body.resetToken, process.env.JWT_USER_ACTIONS_SECRET);
+            next();
+        }catch(err){
+            res.status(401).json({
+                status: 401,
+                message: "Token not found or invalid"
+            });
+            return;
+        }
+    };
+}
