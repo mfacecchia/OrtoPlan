b4f3508f4f82a37a393626cb5508b63af1bf703a
facecchiamarco <facecchiamarco@icloud.com>
2024-07-12 22:06:06 +0200

    Backend: update password reset middleware to check if the reset token exists

--- backend/src/middlewares/resetPassword.middleware.js
+++ backend/src/middlewares/resetPassword.middleware.js
@@ -1,11 +1,20 @@
 import { validateJWT } from "../auth/jwt.auth.js";
 import 'dotenv/config';
+import decodeToken from "../jwt/decode.jwt.js";
+import prisma from "../../db/prisma.db.js";
 
 
 export default function isResetTokenValid(){
     return async (req, res, next) => {
         try{
             await validateJWT(req.body.resetToken, process.env.JWT_USER_ACTIONS_SECRET);
+            const decodedToken = decodeToken(req.body.resetToken, false);
+            const resetPasswordMessage = await prisma.userAction.findUnique({
+                where: {
+                    messageID: decodedToken.messageID
+                }
+            });
+            req.userEmail = resetPasswordMessage.email;
             next();
         }catch(err){
             res.status(401).json({
