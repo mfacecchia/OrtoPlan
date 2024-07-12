3019401ae5d9cb1f66ed59cb7ff5136157bd7f26
facecchiamarco <facecchiamarco@icloud.com>
2024-07-12 22:02:22 +0200

    Backend: update email verification middleware to check if the verification token exists

--- backend/src/middlewares/emailVerification.middleware.js
+++ backend/src/middlewares/emailVerification.middleware.js
@@ -1,11 +1,20 @@
 import { validateJWT } from "../auth/jwt.auth.js";
 import 'dotenv/config';
+import prisma from "../../db/prisma.db.js";
+import decodeToken from "../jwt/decode.jwt.js";
 
 
 export default function isVerificationTokenValid(){
     return async (req, res, next) => {
         try{
             await validateJWT(req.body.verificationToken, process.env.JWT_USER_ACTIONS_SECRET);
+            const decodedToken = decodeToken(req.body.verificationToken, false);
+            const resetPasswordMessage = await prisma.userAction.findUnique({
+                where: {
+                    messageID: decodedToken.messageID
+                }
+            });
+            req.userEmail = resetPasswordMessage.email;
             next();
         }catch(err){
             res.status(401).json({
