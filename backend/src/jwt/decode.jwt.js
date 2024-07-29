2a1a70b38b343beb82a619d2acba149f990f0466
facecchiamarco <facecchiamarco@icloud.com>
2024-07-29 18:59:17 +0200

    Backend: update jwt decode function to remove 'Bearer' prefix truncation line

--- backend/src/jwt/decode.jwt.js
+++ backend/src/jwt/decode.jwt.js
@@ -2,7 +2,6 @@ import jwt from 'jsonwebtoken';
 
 
 export default function decodeToken(token, getFullToken = false){
-    if(token.includes('Bearer ')) token = token.replace('Bearer ', '');
     const decodedToken = jwt.decode(token, { complete: getFullToken });
     return decodedToken;
 }
